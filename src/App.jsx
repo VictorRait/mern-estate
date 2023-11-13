078e6bc9e49d8f72ae1a02be14584d77830eb056
VictorRait <victorlemuelrait@gmail.com>
2023-11-13 14:20:59 +0800

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
+import AppLayout from "./components/AppLayout";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
@@ -8,12 +9,13 @@ function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/sign-in" element={<SignIn />} />
-				<Route path="/sign-up" element={<SignUp />} />
-				<Route path="/about" element={<About />} />
-				<Route path="/profile" element={<Profile />} />
-				<Route path="/" element={<Home />} />
+				<Route path="/" element={<AppLayout />}>
+					<Route path="/home" element={<Home />} />
+					<Route path="/sign-in" element={<SignIn />} />
+					<Route path="/sign-up" element={<SignUp />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/profile" element={<Profile />} />
+				</Route>
 			</Routes>
 		</BrowserRouter>
 	);
