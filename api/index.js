0dcefb528e58c136bdd2b54a1a15871b4415c6f1
VictorRait <victorlemuelrait@gmail.com>
2023-11-24 15:48:17 +0800

    added env PORT

--- api/index.js
+++ api/index.js
@@ -21,8 +21,8 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.listen(3000, () => {
-	console.log("Server is running on port 3000");
+app.listen(process.env.PORT, () => {
+	console.log("Server is running on port " + process.env.PORT);
 });
 
 app.get("/", (req, res) => {});
