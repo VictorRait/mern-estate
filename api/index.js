b4a4b56eb0b8fe589b8f09f544153d345f1a1e74
VictorRait <victorlemuelrait@gmail.com>
2023-11-24 18:24:24 +0800

    removed uneccessary app.get /

--- api/index.js
+++ api/index.js
@@ -25,8 +25,6 @@ app.listen(3000, () => {
 	console.log("Server is running on port 3000");
 });
 
-app.get("/", (req, res) => {});
-
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
