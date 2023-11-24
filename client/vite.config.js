fed8d77b37e790e0759d41f902ea61f7c8748def
VictorRait <victorlemuelrait@gmail.com>
2023-11-24 15:51:29 +0800

    updated proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
 	server: {
 		proxy: {
 			"/api": {
-				target: "http://localhost:3000",
-				secure: false,
+				target: "https://mern-estate-x8am.onrender.com/",
+				secure: true,
 			},
 		},
 	},
